7d762fca2b33c12d1210d9d92ce7cd1a4cc09fbd
Eduardo Soares <eduardoandre.mcc@gmail.com>
2021-07-04 20:19:54 -0300

    Got weather from API

--- src/main/java/com/ifood/demo/controllers/ControllerWeather.java
+++ src/main/java/com/ifood/demo/controllers/ControllerWeather.java
@@ -1,24 +1,34 @@
 package com.ifood.demo.controllers;
 
-import com.ifood.demo.service.ServiceWeather;
+import com.ifood.demo.models.OpenWeather;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/weather")
 public class ControllerWeather {
 
-    @Autowired
-    ServiceWeather weatherService;
+    @Value("${api.key}")
+    private String apiKey;
+
+    final String URL = "https://api.openweathermap.org/data/2.5/weather";
 
     @GetMapping(value = "/{city}")
-    public String getWeatherByCity(@PathVariable String city) {
-        return weatherService.getWeather(city);
+    public OpenWeather getWeatherByCity(@PathVariable String city) {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<OpenWeather> weather = restTemplate.getForEntity(URL + "?appid=" + apiKey + "&q=" + city,
+                OpenWeather.class);
+
+        return weather.getBody();
     }
 }
