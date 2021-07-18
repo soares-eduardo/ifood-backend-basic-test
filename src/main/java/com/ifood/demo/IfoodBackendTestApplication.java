c3fec83f13d4f0a35629e2b70453eaba827adbfd
Eduardo André Soares <eduardoandre.mcc@gmail.com>
2021-07-17 22:22:59 -0300

    Added Swagger dependency.

--- src/main/java/com/ifood/demo/IfoodBackendTestApplication.java
+++ src/main/java/com/ifood/demo/IfoodBackendTestApplication.java
@@ -1,8 +1,13 @@
 package com.ifood.demo;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCaching
@@ -10,5 +15,14 @@ public class IfoodBackendTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IfoodBackendTestApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI generateCustomOpenAPI() {
+		return new OpenAPI().info(new Info()
+				.title("iFood 3 - Backend Basic Test")
+				.version("1.0")
+				.termsOfService("http://swagger.io/terms")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
 }
